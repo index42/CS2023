911e8a28c79a3def68fbfaf94bf752901628526c
Max Hu <Max_Hu@wistron.com>
2023-08-07 10:58:02 +0800

    add project

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System;
+
+class Hello
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World1");
+        Console.WriteLine("Hello, World2");
+        Seasons t = Seasons.All;
+        Console.WriteLine(Convert.ToInt32(t));
+    }
+}
+
+public enum Seasons
+{
+    None = 0,
+    Summer = 1,
+    Autumn = 2,
+    Winter = 4,
+    Spring = 8,
+    All = Summer | Autumn | Winter | Spring
+}
--- head.md
+++ head.md
@@ -1,3 +1,5 @@
 # head
 
-first step
+## Microsoft
+
+C# 程序在 .NET 上运行，而 .NET 是名为公共语言运行时 (CLR) 的虚执行系统和一组类库。 CLR 是 Microsoft 对公共语言基础结构 (CLI) 国际标准的实现。 CLI 是创建执行和开发环境的基础，语言和库可以在其中无缝地协同工作
